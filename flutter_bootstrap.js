2dd72df8c9267b98c7516147952e2a1296c88e66
Guna1237 <gunaratnam1987@gmail.com>
2026-05-09 18:02:00 +0000

    Deploying to gh-pages from @ Guna1237/tomato@3742d1615fd14c552924880758b432fe7586cfea 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1592715997" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1682239732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
